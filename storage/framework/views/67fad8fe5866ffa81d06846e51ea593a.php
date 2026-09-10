4384b2f7b6c80bea77ed3b07c875dd3374030be6
z <z@local>
2026-09-10 09:58:14 +0000

    chore: clean up leaked storage artifacts
    The previous commit accidentally tracked compiled view files, sessions and
    cache data. This restores the .gitignore files in storage/framework/* so
    those stay out of version control.

--- storage/framework/views/67fad8fe5866ffa81d06846e51ea593a.php
+++
@@ -1,7 +0,0 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-<url><loc><?php echo e(route('categories.show', ['category' => $category['slug']])); ?></loc><lastmod><?php echo e($category['lastmod']); ?></lastmod></url>
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</urlset>
-<?php /**PATH /home/z/my-project/agahi/resources/views/seo/categories-sitemap.blade.php ENDPATH**/ ?>
